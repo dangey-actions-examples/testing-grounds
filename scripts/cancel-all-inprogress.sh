7814246f3cea7663076b62919cafa4a662fc5638
Dangey <32312177+Dangey@users.noreply.github.com>
2023-07-15 05:02:53 +0000

    Tested and adjusted script

--- scripts/cancel-all-inprogress.sh
+++ scripts/cancel-all-inprogress.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
-IDS=$(
-  curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $TOKEN"\
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs?status=in_progress |
-  jq .workflow_runs[].id
-)
+if [ -z "$TOKEN" ]
+then
+  echo "error - Lacking user permission to perform cancel"
+else
+  IDS=$(
+    curl -L --no-progress-meter \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs?status=in_progress |
+    jq .workflow_runs[].id
+  )
+fi
 
 if [ -z "$IDS" ]
 then
+  echo "There are no in_progress workflow runs currently."
+else
   for ID in $IDS
   do
     echo "Requesting cancellation for ... run $ID"
-    curl -L \
-    -X POST \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $TOKEN"\
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$ID/cancel
-    echo "done - Cancelled run $ID"
+    response=$(
+      curl -L --no-progress-meter \
+      -X POST \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer $TOKEN" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$ID/cancel
+    )
+    echo "[done] - Attempted to cancel run $ID"
   done
-else
-  echo "There are no `in_progress` workflow runs currently."
 fi
