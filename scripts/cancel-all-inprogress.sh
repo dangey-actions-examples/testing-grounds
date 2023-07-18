28d06028e72e10052b327bc74b71a3bdcb925c0d
Dangey <32312177+Dangey@users.noreply.github.com>
2023-07-17 19:54:43 -0500

    Update cancel-all-not-completed.sh

--- scripts/cancel-all-inprogress.sh
+++ scripts/cancel-all-inprogress.sh
@@ -8,7 +8,7 @@ else
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs?status=in_progress |
+    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs?status!=completed |
     jq .workflow_runs[].id
   )
 fi
