ce44cc0a44eaf0979f5fc05480b78b07f8ce7b44
Dangey <32312177+Dangey@users.noreply.github.com>
2023-08-07 20:22:10 -0500

    Update cancel-all-inprogress.sh

--- scripts/cancel-all-inprogress.sh
+++ scripts/cancel-all-inprogress.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
+
 if [ -z "$TOKEN" ]
 then
-  echo "error - Lacking user permission to perform cancel"
-else
-  IDS=$(
-    curl -L --no-progress-meter \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $TOKEN" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs?status=in_progress |
-    jq .workflow_runs[].id
-  )
+  echo "::error::Missing permission - Please set auth token in TOKEN variable."
+  exit 1
 fi
+
+IDS=$(
+  curl -L --no-progress-meter \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs?status=in_progress |
+  jq .workflow_runs[].id
+)
 
 if [ -z "$IDS" ]
 then
-  echo "There are no in_progress workflow runs currently."
+  echo "::notice::There are no in_progress workflow runs currently."
 else
   for ID in $IDS
   do
-    echo "Requesting cancellation for ... run $ID"
+    echo "[start] - Requesting cancellation for ... run $ID"
     response=$(
       curl -L --no-progress-meter \
       -X POST \
